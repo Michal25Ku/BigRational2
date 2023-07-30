883c130dd00be258fde25bc1bdab83c7d0b54bd1
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-07-30 15:30:26 +0200

    Add new unit Tests BigRationalConversionsUnitTests, override explicit operator for: byte, sbyte, short, ushort, uint, ulong, long, develop methods for them like: ToByte, ToSbyte, ToShort, ToUshort, ToUint, ToLong, ToUlong  and implicit operator for: byte, sbyte, short, ushort, uint, long, ulong

---
+++ RationalLib/BigRationalConversions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalLib
+{
+    public readonly partial struct BigRational
+    {
+        public static implicit operator BigRational(byte n) => new BigRational(n,1);
+        public static implicit operator BigRational(sbyte n) => new BigRational(n,1);
+        public static implicit operator BigRational(short n) => new BigRational(n,1);
+        public static implicit operator BigRational(ushort n) => new BigRational(n,1);
+        public static implicit operator BigRational(uint n) => new BigRational(n,1);
+        public static implicit operator BigRational(long n) => new BigRational(n,1);
+        public static implicit operator BigRational(ulong n) => new BigRational(n,1);
+
+        public static explicit operator byte(BigRational b) => ((b.Numerator / b.Denominator) < 0) ? (byte)0 : (byte)(b.Numerator / b.Denominator);
+        public static explicit operator sbyte(BigRational b) => (sbyte)(b.Numerator / b.Denominator);
+        public static explicit operator short(BigRational b) => (short)(b.Numerator / b.Denominator);
+        public static explicit operator ushort(BigRational b) => ((b.Numerator / b.Denominator) < 0) ? (ushort)0 : (ushort)(b.Numerator / b.Denominator);
+        public static explicit operator uint(BigRational b) => ((b.Numerator / b.Denominator) < 0) ? 0 : (uint)(b.Numerator / b.Denominator);
+        public static explicit operator long(BigRational b) => (long)(b.Numerator / b.Denominator);
+        public static explicit operator ulong(BigRational b) => ((b.Numerator / b.Denominator) < 0) ? 0 : (ulong)(b.Numerator / b.Denominator);
+        public byte ToByte(IFormatProvider? provider) => (byte)this;
+        public sbyte ToSbyte(IFormatProvider? provider) => (sbyte)this;
+        public short ToShort(IFormatProvider? provider) => (short)this;
+        public ushort ToUshort(IFormatProvider? provider) => (ushort)this;
+        public uint ToUint(IFormatProvider? provider) => (uint)this;
+        public long ToLong(IFormatProvider? provider) => (long)this;
+        public ulong ToUlong(IFormatProvider? provider) => (ulong)this;
+
+        public static explicit operator int(BigRational b) => (int)(b.Numerator / b.Denominator);
+        public static explicit operator double(BigRational b) => (double)(b.Numerator / b.Denominator);
+        public static explicit operator float(BigRational b) => (float)(b.Numerator / b.Denominator);
+        public static explicit operator decimal(BigRational b) => (decimal)(b.Numerator / b.Denominator);
+
+        public int ToInt(IFormatProvider? provider) => (int)this;
+        public double ToDouble(IFormatProvider? provider) => (double)this;
+        public float ToSingle(IFormatProvider? provider) => (float)this;
+        public decimal ToDecimal(IFormatProvider? provider) => (decimal)this;
+
+    }
+}
---
+++ RationalUnitTest/BigRationalConversionsUnitTests.cs
@@ -0,0 +1,208 @@
+﻿using RationalLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalUnitTest
+{
+    [TestClass]
+    public class BigRationalConversionsUnitTests
+    {
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, 0)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, 0)]
+        public void Test_Conversions_Explicit_To_Byte(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            byte l = (byte)b;
+            Assert.AreEqual((byte)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, -2)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, -2)]
+        public void Test_Conversions_Explicit_To_Sbyte(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            sbyte l = (sbyte)b;
+            Assert.AreEqual((sbyte)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, -2)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, -2)]
+        public void Test_Conversions_Explicit_To_Short(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            short l = (short)b;
+            Assert.AreEqual((short)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, 0)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, 0)]
+        public void Test_Conversions_Explicit_To_Ushort(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            ushort l = (ushort)b;
+            Assert.AreEqual((ushort)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, 0)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, 0)]
+        public void Test_Conversions_Explicit_To_Uint(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            uint l = (uint)b;
+            Assert.AreEqual((uint)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, -2)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, -2)]
+        public void Test_Conversions_Explicit_To_Long(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            long l = (long)b;
+            Assert.AreEqual((long)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 2, 0)]
+        [DataRow(2, 2, 1)]
+        [DataRow(4, 2, 2)]
+        [DataRow(-2, 1, 0)]
+        [DataRow(-1, 2, 0)]
+        [DataRow(-2, -1, 2)]
+        [DataRow(2, -1, 0)]
+        public void Test_Conversions_Explicit_To_Ulong(int n, int d, int e)
+        {
+            BigRational b = new BigRational(n, d);
+            ulong l = (ulong)b;
+            Assert.AreEqual((ulong)e, l);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        public void Test_Conversions_Implicit_To_Byte(int n, int en)
+        {
+            BigRational b = n;
+   
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((byte)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        [DataRow(-2, -2)]
+        [DataRow(-1, -1)]
+        public void Test_Conversions_Implicit_To_Sbyte(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((sbyte)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        [DataRow(-2, -2)]
+        [DataRow(-1, -1)]
+        public void Test_Conversions_Implicit_To_Short(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((short)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        public void Test_Conversions_Implicit_To_Ushort(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((ushort)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        public void Test_Conversions_Implicit_To_Uint(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((uint)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        public void Test_Conversions_Implicit_To_Ulong(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((ulong)1, b.Denominator);
+        }
+
+        [DataTestMethod]
+        [DataRow(1, 1)]
+        [DataRow(2, 2)]
+        [DataRow(4, 4)]
+        [DataRow(-2, -2)]
+        [DataRow(-1, -1)]
+        public void Test_Conversions_Implicit_To_Long(int n, int en)
+        {
+            BigRational b = n;
+
+            Assert.AreEqual(en, b.Numerator);
+            Assert.AreEqual((long)1, b.Denominator);
+        }
+    }
+}
