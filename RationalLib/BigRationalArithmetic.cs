9269425b2dfc690aac08df8af84f807fa306a561
Michał Kuciak <michal.kuciak@microsoft.wsei.edu.pl>
2023-08-01 22:05:41 +0200

    Add Plus method to BigRational

---
+++ RationalLib/BigRationalArithmetic.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalLib
+{
+    public readonly partial struct BigRational
+    {
+        public BigRational Plus(BigRational other)
+        {
+            if (IsNaN(this) || IsNaN(other))
+                return NaN;
+
+            if (IsPositiveInfinity(this) && IsPositiveInfinity(other))
+                return zero;
+
+            return new BigRational((this.Numerator * other.Denominator) + (other.Numerator * this.Denominator), (this.Denominator * other.Denominator));
+        }
+    }
+}
---
+++ RationalUnitTest/BigRationalArithmeticUnitTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RationalUnitTest
+{
+    [TestClass]
+    public class BigRationalArithmeticUnitTests
+    {
+        [TestMethod]
+        [DataTestMethod]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+        [DataRow(1, 2, 1, 2, 1, 1)]
+    }
+}
